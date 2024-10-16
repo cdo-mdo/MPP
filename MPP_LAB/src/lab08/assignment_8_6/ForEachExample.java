af4d93f44ac7c9cf80bc7222fce6754c34f5fb5d
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-16 13:15:48 -0500

    submit assignment_8_6

--- MPP_LAB/src/lab08/assignment_8_6/ForEachExample.java
+++ MPP_LAB/src/lab08/assignment_8_6/ForEachExample.java
@@ -10,21 +10,6 @@ public class ForEachExample {
         List<String> list = Arrays.asList("Hello there", "Goodbye", "Back soon", "Away", "On Vacation",
                 "Everywhere you want to be");
 
-        // print each element of the list in upper case format
-//		Consumer<String> consumer = new Consumer<String>() {
-//
-//			@Override
-//			public void accept(String t) {
-//				System.out.println(t.toUpperCase());
-//			}
-//			
-//		};
-
-        list.forEach((t) -> {
-            t.toUpperCase(); 
-            System.out.println(t.toUpperCase());
-        });
-
+        list.forEach(String::toUpperCase);
     }
-
 }
