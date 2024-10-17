55916aef05e7c162d981d6c29bf063d15add2253
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-17 05:48:37 -0500

    review assignment_9_1 for submit

--- MPP_LAB/src/lab09/assignment_9_1/business/Main.java
+++ MPP_LAB/src/lab09/assignment_9_1/business/Main.java
@@ -25,14 +25,10 @@ public class Main {
         mems.addAll(members);
         
         // implement
-        List<LibraryMember> sublist = mems.stream()
+        return mems.stream()
             .filter(member -> member.getAddress().getZip().contains("" + '3'))
+            .map(member -> member.getMemberId())
             .collect(Collectors.toList());
-        List<String> memberIds = new ArrayList<>();
-        sublist.forEach(member -> memberIds.add(member.getMemberId()));
-        
-        return memberIds;
-
     }
 
     // Returns a list of all isbns of books having at least two copies
@@ -41,15 +37,14 @@ public class Main {
         Collection<Book> books = da.readBooksMap().values();
         List<Book> bs = new ArrayList<>();
         bs.addAll(books);
+
+//        bs.forEach(book -> System.out.println(book.getNumCopies()));
         
         // implement
-        List<Book> subList = bs.stream()
+        return bs.stream()
             .filter(book -> book.getNumCopies() >= 2)
+            .map(book -> book.getIsbn())
             .collect(Collectors.toList());
-        List<String> isbns = new ArrayList<>();
-        subList.forEach(book -> isbns.add(book.getIsbn()));
-        
-        return isbns;
 
     }
 
@@ -59,15 +54,12 @@ public class Main {
         Collection<Book> books = da.readBooksMap().values();
         List<Book> bs = new ArrayList<>();
         bs.addAll(books);
-        
+
         // implement
-        List<Book> subList = bs.stream()
+        return bs.stream()
             .filter(book -> book.getAuthors().size() > 1)
+            .map(book -> book.getIsbn())
             .collect(Collectors.toList());
-        List<String> isbns = new ArrayList<>();
-        subList.forEach(book -> isbns.add(book.getIsbn()));
-        
-        return isbns;
 
     }
 
