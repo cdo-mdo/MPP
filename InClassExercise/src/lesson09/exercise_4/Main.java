e5dd4d16da16614b60b1ee84bee357464d376314
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-17 13:31:30 -0500

    update

--- InClassExercise/src/lesson09/exercise_4/Main.java
+++ InClassExercise/src/lesson09/exercise_4/Main.java
@@ -1,11 +1,17 @@
 package lesson09.exercise_4;
 
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
 	//Use the reduce method to produce a single space-separated String
 	public static void main(String[] args) {
-		Stream strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
+		Stream<String> strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
+		
+		
+		  strings.reduce("", (a, b) -> a + " " + b);
+		
+		  strings.collect(Collectors.joining(", "));
 		
 	}
 
--- InClassExercise/src/lesson09/exercise_5/Main.java
+++ InClassExercise/src/lesson09/exercise_5/Main.java
@@ -1,24 +1,38 @@
 package lesson09.exercise_5;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-	//Use DoubleSummaryStatistics to output the top test score,
-	//the lowest test score, and the average of all test scores
-	public static void main(String[] args) {
-		List<ExamData> data = new ArrayList<ExamData>() {
-			{
-				add(new ExamData("George", 91.3));
-				add(new ExamData("Tom", 88.9));
-				add(new ExamData("Rick", 80));
-				add(new ExamData("Harold", 90.8));
-				add(new ExamData("Ignatius", 60.9));
-				add(new ExamData("Anna", 77));
-				add(new ExamData("Susan", 87.3));
-				add(new ExamData("Phil", 99.1));
-				add(new ExamData("Alex", 84));
-			}
-		};
-	}
+    // Use DoubleSummaryStatistics to output the top test score,
+    // the lowest test score, and the average of all test scores
+    public static void main(String[] args) {
+        List<ExamData> data = new ArrayList<ExamData>() {
+            /**
+             * 
+             */
+            private static final long serialVersionUID = 279779774152589263L;
+
+            {
+                add(new ExamData("George", 91.3));
+                add(new ExamData("Tom", 88.9));
+                add(new ExamData("Rick", 80));
+                add(new ExamData("Harold", 90.8));
+                add(new ExamData("Ignatius", 60.9));
+                add(new ExamData("Anna", 77));
+                add(new ExamData("Susan", 87.3));
+                add(new ExamData("Phil", 99.1));
+                add(new ExamData("Alex", 84));
+            }
+        };
+        
+        DoubleSummaryStatistics summary = data.stream()
+            .collect(Collectors.summarizingDouble(ExamData::getTestScore));
+        
+        System.out.println(summary.getAverage());
+        System.out.println(summary.getMax());
+        System.out.println(summary.getMin());
+        System.out.println(summary);
+    }
 
 }
