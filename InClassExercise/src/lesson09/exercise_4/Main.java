3a1b69a46bd780e627e6952704510de228e2289f
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-18 05:20:10 -0500

    update

--- InClassExercise/src/lesson09/exercise_4/Main.java
+++ InClassExercise/src/lesson09/exercise_4/Main.java
@@ -4,15 +4,14 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
-	//Use the reduce method to produce a single space-separated String
-	public static void main(String[] args) {
-		Stream<String> strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
-		
-		
-		  strings.reduce("", (a, b) -> a + " " + b);
-		
-		  strings.collect(Collectors.joining(", "));
-		
-	}
+    // Use the reduce method to produce a single space-separated String
+    public static void main(String[] args) {
+        Stream<String> strings = Stream.of("A", "good", "day", "to", "write", "some", "Java");
+
+//        System.out.println(strings.reduce("", (a, b) -> a + " " + b));
+
+		System.out.println(strings.collect(Collectors.joining(" ")));
+
+    }
 
 }
