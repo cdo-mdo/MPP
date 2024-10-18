27ac30edfb472ae9237dc5b3d524a5c7d22d0f35
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-18 05:07:06 -0500

    update

--- LectureCode/src/lesson09/lecture/streamreuse/Reuse.java
+++ LectureCode/src/lesson09/lecture/streamreuse/Reuse.java
@@ -1,4 +1,5 @@
 package lesson09.lecture.streamreuse;
+
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.stream.Collectors;
@@ -8,23 +9,20 @@ import java.util.stream.Stream;
 //part of a larger  lambda expression implementing some type of Function interface,
 //and then gain access to the desired stream with a call to the Function's apply method
 public class Reuse {
-  private static final BiFunction<List<String>, String, Stream<String>> listStartsWith 
-    = (list, letter) -> list.stream()
-                            .filter(name -> name.startsWith(letter));
-                        
-  public int countNumberFriendsStartingWithB() {
-	  return (int) listStartsWith.apply(Folks.friends, "B").count();
-  }
-  public List<String> listEditorsNamesStartingWithNUpperCase() {
-	  return listStartsWith.apply(Folks.editors, "N")
-			               .map(name -> name.toUpperCase())
-			               .collect(Collectors.toList());
-  }
-  
-  
-  public static void main(String[] args) {
-	  Reuse r = new Reuse();
-	  System.out.println(r.countNumberFriendsStartingWithB());
-	  System.out.println(r.listEditorsNamesStartingWithNUpperCase());
-  }
+    private static final BiFunction<List<String>, String, Stream<String>> listStartsWith = (list, letter) -> list
+            .stream().filter(name -> name.startsWith(letter));
+
+    public int countNumberFriendsStartingWithB() {
+        return (int) listStartsWith.apply(Folks.friends, "B").count();
+    }
+
+    public List<String> listEditorsNamesStartingWithNUpperCase() {
+        return listStartsWith.apply(Folks.editors, "N").map(name -> name.toUpperCase()).collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        Reuse r = new Reuse();
+        System.out.println(r.countNumberFriendsStartingWithB());
+        System.out.println(r.listEditorsNamesStartingWithNUpperCase());
+    }
 }
