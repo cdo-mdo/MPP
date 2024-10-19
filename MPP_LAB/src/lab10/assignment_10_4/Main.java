7f8b1c76173360c2c43ae665ef614580476c3b74
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-19 04:07:06 -0500

    implement assignment_10_4

--- MPP_LAB/src/lab10/assignment_10_4/Main.java
+++ MPP_LAB/src/lab10/assignment_10_4/Main.java
@@ -3,6 +3,7 @@ package lab10.assignment_10_4;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.function.Predicate;
 
 public class Main {
 
@@ -17,7 +18,24 @@ public class Main {
                 .map(e -> fullName(e)).sorted().collect(Collectors.joining(", ")));
 
     }
+    
+    public String asString(List<Employee> list) {
+        return list.stream()
+                .filter(e -> (e.getSalary() > 100000))
+                .filter(e -> (e.getLastName().charAt(0) > 'M'))
+                .map(e -> fullName(e))
+                .sorted()
+                .collect(Collectors.joining(", "));
+    }
+    
+    void printEmps(List<Employee> list) {
+        System.out.println(asString(list));
+    }
 
+    public Predicate<Employee> salaryGreaterThan100000 = a ->  a.getSalary() > 100000;
+    public Predicate<Employee> lastNameAfterM = a -> a.getLastName().charAt(0) > 'M';
+    
+    
     private static String fullName(Employee e) {
         return e.getFirstName() + " " + e.getLastName();
     }
