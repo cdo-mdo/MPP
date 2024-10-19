3b229923eb7a54f826d33b6a3abde10e48a0e3af
Manh Cuong Do <domanhcuong.cdo@gmail.com>
2024-10-19 06:48:02 -0500

    review assignment_10_2 for submit

--- MPP_LAB/src/lab10/assignment_10_2/FixThisSoln.java
+++ MPP_LAB/src/lab10/assignment_10_2/FixThisSoln.java
@@ -2,10 +2,9 @@ package lab10.assignment_10_2;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public class FixThis {
+public class FixThisSoln {
     /* This method creates a compiler error -- try to fix it */
     List<String> processList(List<String> list) {
 //        return list.stream().map(x -> wrapper(x)).collect(Collectors.toList());
@@ -41,9 +40,11 @@ public class FixThis {
     }
 
     public static void main(String[] args) {
-        FixThis ft = new FixThis();
+        FixThisSoln ft = new FixThisSoln();
         List<String> words1 = Arrays.asList("not", "too", "big", "yet");
         System.out.println(ft.processList(words1));
+        List<String> words2 = Arrays.asList("not1", "too", "big", "yet");
+        System.out.println(ft.processList(words2));
     }
 
     class InputTooLongException extends Exception {
